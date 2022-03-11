0f57b3c9853b682ff8d423ea777ba1bd4938defb
andreoliveira1193 <58228143+andreoliveira1193@users.noreply.github.com>
2022-03-10 23:31:41 -0300

    Delete src directory

--- src/main/java/com/dio_class/devweek/Repo/FaixaEtariaRepo.java
+++
@@ -1,9 +0,0 @@
-package com.dio_class.devweek.Repo;
-
-import com.dio_class.devweek.Entity.FaixaEtaria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FaixaEtariaRepo extends JpaRepository <FaixaEtaria, Long> {
-
-
-}
